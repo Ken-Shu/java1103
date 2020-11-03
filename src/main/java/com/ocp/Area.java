ae666eccca69cd0dcb7e3af5730adf9604284711
MB-study <MB-study@192.168.1.101>
2020-11-03 21:43:59 +0800

    HELLO JAVA

--- src/main/java/com/ocp/Area.java
+++ src/main/java/com/ocp/Area.java
@@ -4,8 +4,14 @@ package com.ocp;
 public class Area {
     int w;
     int h;
+    //第一種方法
     void calcAndPrint(){
         int sum= w*h;
         System.out.println(sum);
     }
+    //第二種方法
+     int calcNotPrint(){
+        int sum= w*h;
+        return sum;
+     }
 }
