ee0302e3f25892cebf88e85ffc9d81724106e88a
ABC <ABC@DESKTOP-SVHVPM8>
2020-11-06 00:13:43 +0800

    Homework

--- src/main/java/com/opc2/PrintStar3.java
+++ src/main/java/com/opc2/PrintStar3.java
@@ -1,11 +1,28 @@
-
 package com.opc2;
 
 public class PrintStar3 {
+
     public static void main(String[] args) {
         int x = 4;
         int y = 7;
-        for (int i = 1 ; i<= x ; i ++)
-            for(int j = i ; j)
+        //*
+        //**
+        //***
+        //***
+        //***
+        //**
+        //*       
+        for (int p = 1; p <= x; p++) {
+            for (int m = 1; m <= p; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 7; i > x; i--) {
+            for (int j = 4; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
