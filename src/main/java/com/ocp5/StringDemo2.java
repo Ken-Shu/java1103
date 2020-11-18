b8abe2a54c5d742f5ddfbd07f7f5e13baca41fab
ABC <ABC@DESKTOP-SVHVPM8>
2020-11-18 23:06:32 +0800

    Homework

--- src/main/java/com/ocp5/StringDemo2.java
+++ src/main/java/com/ocp5/StringDemo2.java
@@ -3,10 +3,13 @@ package com.ocp5;
 
 public class StringDemo2 {
     public static void main(String[] args) {
-        String s1 =new String("Java");
-        String s2 = "Java";
+        String s1 =new String("Java"); //new string 放置在 stack區
+        String s2 = "Java"; // 此寫法放置在Heap區
         //s1=s1.intern();//intern()將字串移入到String pool
         s1=s2;//改變s1的指向位置
         System.out.println(s1==s2);
+        //也可用:
+        //System.out.println(s1.intern()==s2);
+        System.out.println(s1.equals(s2)); //equals 只比較字串相同 不比較擺放區域
     }
 }
