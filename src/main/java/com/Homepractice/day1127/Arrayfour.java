984c44451ed9626ec0bde1ca3b388941c8b082f7
ABC <ABC@DESKTOP-SVHVPM8>
2020-11-27 23:41:28 +0800

    Homework

---
+++ src/main/java/com/Homepractice/day1127/Arrayfour.java
@@ -0,0 +1,19 @@
+package com.Homepractice.day1127;
+
+import java.util.Arrays;
+
+public class Arrayfour {
+    public static void main(String[] args) {
+        int[][] m = {{1,2,3},{3,4,5}}; //[2].[3]
+        for (int i = 0; i < m.length; i++) {
+            int[] p= m[i];
+            for (int j=0; j<m[i].length;j++){
+                int z = p [j];
+               System.out.println(z);
+            }
+             
+        }
+       
+    }
+    
+}
--- src/main/java/com/Homepractice/day1127/Arraythree.java
+++ src/main/java/com/Homepractice/day1127/Arraythree.java
@@ -12,7 +12,7 @@ public class Arraythree {
 //        array[0][1][0] = 4;
 //        array[1][1][0] = 5;
 //        array[2][1][0] = 6;
-        int[][][] array2 = {{{0, 1, 2}, {3, 4, 5}, {6,7,8}}}; //3.1.1
+        int[][][] array2 = {{{0, 1, 2}, {3, 4, 5}, {6,7,8}}}; //[1][3][3]
         for (int i = 0; i < array2.length; i++) {
             int [][] arra = array2[i];
             for (int j = 0; j < array2[i].length; j++) {
