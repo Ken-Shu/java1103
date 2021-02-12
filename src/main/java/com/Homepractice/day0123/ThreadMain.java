5363c5c68fbbd201b88b7b6e75df4037d99e8386
ABC <ABC@DESKTOP-SVHVPM8>
2021-02-12 23:01:30 +0800

    Homework

--- src/main/java/com/Homepractice/day0123/ThreadMain.java
+++ src/main/java/com/Homepractice/day0123/ThreadMain.java
@@ -9,12 +9,10 @@ public class ThreadMain {
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(3);
         service.submit(new Fight());
-        
-        for (int i = 0; i < 5; i++) {
-        service.submit(new MonsterAttack());
-        service.submit(new HeroAttack());
-         
-        }
+       
+            service.submit(new MonsterAttack());
+            service.submit(new HeroAttack());
+ 
         service.shutdown(); 
     }
 }
