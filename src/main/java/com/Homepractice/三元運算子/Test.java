b524e93af134885a7ac07b071b797d7fd8aa354d
ABC <ABC@DESKTOP-SVHVPM8>
2021-02-17 21:56:56 +0800

    Homework

--- src/main/java/com/Homepractice/三元運算子/Test.java
+++ src/main/java/com/Homepractice/三元運算子/Test.java
@@ -3,8 +3,8 @@ package com.Homepractice.三元運算子;
 public class Test {
     public static void main(String[] args) {
         String i = "Jim";
-        //三元運算子 若比較出來為true 則 帶入冒號左方
-        //因為左方為 false 則答案為 else 後面
+        //三元運算子 若比較出來為true 
+        //則因為true 放在後方 所以答案為 後方之 NG
         if(i.equals("Jim")?false:true){
             System.out.println("OK");
         }else{System.out.println("NG");
---
+++ src/main/java/com/Homepractice/三元運算子/Test2.java
@@ -0,0 +1,12 @@
+
+package com.Homepractice.三元運算子;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int x = 10;
+        if((x>5)?false:true){
+            System.out.println("false1");
+    }else System.out.println("true1");
+    }
+    
+}
