60fe07eca746a9d9fdbbb21883aad68ed931a88e
ABC <ABC@DESKTOP-SVHVPM8>
2021-02-22 22:21:20 +0800

    Homework

--- src/main/java/com/Homepractice/OCA物件導向/FatherExtends.java
+++ src/main/java/com/Homepractice/OCA物件導向/FatherExtends.java
@@ -2,7 +2,17 @@
 package com.Homepractice.OCA物件導向;
 
 public class FatherExtends {
-    public void brush(){
-        System.out.println("爸爸洗澡");
-    }
+    public int money;
+   public void brush(){
+       System.out.println("爸爸洗澡!");
+   }
+   public int setmoney(int money){
+       this.money = money;
+       int father = 0;
+       father += money;
+       return money;
+   }
+   public int getmoney(){
+       return this.money;
+}
 }
--- src/main/java/com/Homepractice/OCA物件導向/SonExtendFather.java
+++ src/main/java/com/Homepractice/OCA物件導向/SonExtendFather.java
@@ -2,20 +2,22 @@ package com.Homepractice.OCA物件導向;
 
 public class SonExtendFather extends FatherExtends{
     public static void main(String[] args) {
-        //兒子洗澡
-        SonExtendFather so = new SonExtendFather();
-        //兒子洗澡 兒子把自己的方法 丟給了爸爸
-        FatherExtends fo = new SonExtendFather();
-        //爸爸洗澡 爸爸依然照自己的方法 沒有被兒子的方法影響
-        FatherExtends foo = new FatherExtends();
-        so.brush();
-        fo.brush();
-        foo.brush();
+        FatherExtends fat = new SonExtendFather();
+        fat.setmoney(500);
+        System.out.println("爸爸有"+fat.money);
+        //因為上述 fat 有500 所以同為FatherExtends 的 money 物件
+        //但是因為 new 了一個新的 son 物件 所以同時也另外新建了一個 money物件
+        //如果將 原先的 money 物件 宣告為 static 的話 則會變成共用物件 
+        //此時 fat 和 son 的 money 都會變成 500
+        SonExtendFather son = new SonExtendFather();
+        System.out.println("爸爸要給兒子200元");
+        int fatherForson = 200;
+        //兒子的錢
+        int son1 = son.setmoney(fatherForson);
+        //爸爸剩下的錢
+        
+        System.out.println("兒子現在有"+son1);
+        System.out.println("爸爸還有"+(fat.getmoney()-son1));
+        
     }
-
-    @Override
-    public void brush() {
-        System.out.println("兒子洗澡");
-    }
-    
 }
