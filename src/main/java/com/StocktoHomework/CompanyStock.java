e0075ae5441453a5a33463d5dded9c29ca764758
ABC <ABC@DESKTOP-SVHVPM8>
2021-03-23 23:25:38 +0800

    Homework

--- src/main/java/com/StocktoHomework/CompanyStock.java
+++ src/main/java/com/StocktoHomework/CompanyStock.java
@@ -2,6 +2,9 @@
 package com.StocktoHomework;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Date;
 import java.util.concurrent.FutureTask;
 import yahoofinance.Stock;
@@ -17,9 +20,37 @@ public class CompanyStock {
  *最高 DECIMAL(10);
  *最低 DECIMAL(10);
 */  
-    Date date = new Date();
-   
+    Date date = new Date();   
     String symbol;
+    
+    private  static Connection conn;
+    static {
+        try {
+            String url = "jdbc:derby://localhost:1527/APP";
+            String user = "app";
+            String password = "app";
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void importToStocksNumber(String url) throws Exception{
+        String stocks = companyNumber(url);
+        String sql = "Insert Into Stock(公司編號,公司名稱,公司股價,公司漲幅)"+"Values(?,?,?,?)";
+        try {
+            PreparedStatement pstmt = conn.prepareCall(sql);{
+            pstmt.clearBatch();
+           pstmt.setString(2, stocks);
+           pstmt.addBatch();
+        
+            pstmt.executeBatch();
+            System.out.println("匯入成功");
+        }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
     
     //公司名稱
      public String companyNumber(String number) throws Exception{
